f6fd256821352d32b10d5690e8e98cce61207f7a
Ioffe Ben <ben.ioffe@aol.com>
2021-11-02 17:54:07 -0400

    Create icws_.py

---
+++ scripts/icws_.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2021, Ben Ioffe (github.com/ioffe-ben). All rights reserved. Copyrights licensed under the BSD 3-Clause License. See the accompanying LICENSE file for terms.
+# Code snippet description: the following snippet shows how to generate a list of users to roles using ICWS API.
+import json
+import requests
+import icws_cloud_authentication # or import icws_premise_authentication for PureConnect Premise (more details: https://github.com/ioffe-ben/icws-code-snippets/blob/main/scripts/icws_premise_authentication.py)
+
+request = requests.get(icws_cloud_authentication.baseURL + icws_cloud_authentication.json_connection_response['sessionId'] + '/configuration/users', headers=icws_cloud_authentication.header, cookies=icws_cloud_authentication.cookie, verify=False)
+json_users = json.loads(request.text)
+
+i = 0
+while i < len(json_users['items']):
+    try:
+        request_user_roles = requests.get(icws_cloud_authentication.baseURL + icws_cloud_authentication.json_connection_response['sessionId'] + '/configuration/users/' + json_users['items'][i]['configurationId']['id'] + '?select=roles', headers=icws_cloud_authentication.header, cookies=icws_cloud_authentication.cookie, verify=False)
+        json_user_roles = json.loads(request_user_roles.text)
+
+        members = ''
+        ii = 0
+        while ii < len(json_user_roles['roles']['effectiveValue']):
+            members += json_user_roles['roles']['effectiveValue'][ii]['id'] + ', '
+            ii += 1
+        print(json_users['items'][i]['configurationId']['id'] + ' (' + members[:-2] + ')')
+        i += 1
+    except:
+        print(json_users['items'][i]['configurationId']['id'] + ' (NA)')
+        i += 1
