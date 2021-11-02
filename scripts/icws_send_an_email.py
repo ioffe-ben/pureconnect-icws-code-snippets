bfc9178c3703996bd43d46535005a0e7e6638ec4
Ioffe Ben <ben.ioffe@aol.com>
2021-11-02 16:07:07 -0400

    Send an email

--- scripts/icws_send_an_email.py
+++ scripts/icws_send_an_email.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2021, Ben Ioffe (github.com/ioffe-ben). All rights reserved. Copyrights licensed under the BSD 3-Clause License. See the accompanying LICENSE file for terms.
 # Code snippet description: the following snippet shows how to send an email using ICWS API.
-
 import json
 import requests
 import icws_cloud_authentication # or import icws_premise_authentication for PureConnect Premise (more details: https://github.com/ioffe-ben/icws-code-snippets/blob/main/scripts/icws_premise_authentication.py)
