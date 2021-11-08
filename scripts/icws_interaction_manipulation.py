810be4da688b413641e0030820210e75f78ead2d
Ioffe Ben <ben.ioffe@aol.com>
2021-11-08 08:52:35 -0500

    How to manipulate live interaction

--- scripts/icws_interaction_manipulation.py
+++ scripts/icws_interaction_manipulation.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021, Ben Ioffe (github.com/ioffe-ben). All rights reserved. Copyrights licensed under the BSD 3-Clause License. See the accompanying LICENSE file for terms.
-# Code snippet description: the following snippet shows how to send an email using ICWS API.
+# Code snippet description: the following snippet shows how to manipulate live interaction using ICWS API.
 import json
 import requests
 import icws_cloud_authentication # or import icws_premise_authentication for PureConnect Premise (more details: https://github.com/ioffe-ben/pureconnect-icws-code-snippets/blob/main/scripts/icws_premise_authentication.py)
@@ -57,6 +57,15 @@ else:
 # Sends DTMF digits to an interaction
 body = {'dtmfDigits': '1'}
 request = requests.post(icws_cloud_authentication.baseURL + icws_cloud_authentication.json_connection_response['sessionId'] + '/interactions/' + interaction_id + '/send-digits', headers=icws_cloud_authentication.header, cookies=icws_cloud_authentication.cookie, data=json.dumps(body), verify=False)
+
+if request.status_code == 200:
+    print(str(request.status_code) + ': the action was successfully performed')
+else:
+    print(str(request.status_code) + ': error')
+    
+# Plays a wave file to the call
+body = {'file': 'name_of_the_wav_file_to_play.wav'}
+request = requests.post(icws_cloud_authentication.baseURL + icws_cloud_authentication.json_connection_response['sessionId'] + '/interactions/' + interaction_id + '/play-wav', headers=icws_cloud_authentication.header, cookies=icws_cloud_authentication.cookie, data=json.dumps(body), verify=False)
 
 if request.status_code == 200:
     print(str(request.status_code) + ': the action was successfully performed')
